5d8985bff29d7820c43c11eea5aa7168103a0284
GenkiHirano <genki86web@gmail.com>
2022-07-16 20:04:15 +0900

    test: サーバーがスコア情報を持たない変更に伴って、テストを修正

--- server_test.go
+++ server_test.go
@@ -7,34 +7,51 @@ import (
 	"testing"
 )
 
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(n string) int {
+	score := s.scores[n]
+	return score
+}
+
 func TestGETPlayers(t *testing.T) {
-    t.Run("returns Pepper's score", func(t *testing.T) {
-        request := newGetScoreRequest("Pepper")
-        response := httptest.NewRecorder()
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+	server := &PlayerServer{&store}
 
-        PlayerServer(response, request)
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
 
-        assertResponseBody(t, response.Body.String(), "20")
-    })
+		server.ServeHTTP(response, request)
 
-    t.Run("returns Floyd's score", func(t *testing.T) {
-        request := newGetScoreRequest("Floyd")
-        response := httptest.NewRecorder()
+		assertResponseBody(t, response.Body.String(), "20")
+	})
 
-        PlayerServer(response, request)
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
 
-        assertResponseBody(t, response.Body.String(), "10")
-    })
+		server.ServeHTTP(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
 }
 
 func newGetScoreRequest(name string) *http.Request {
-    req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
-    return req
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
 }
 
 func assertResponseBody(t testing.TB, got, want string) {
-    t.Helper()
-    if got != want {
-        t.Errorf("response body is wrong, got %q want %q", got, want)
-    }
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
 }
