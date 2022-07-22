e22917aab93fa1e6b51290b7ee81b7ec8e2ee634
GenkiHirano <genki86web@gmail.com>
2022-07-22 17:33:09 +0900

    test: POSTで勝利数を記録できたかのテストを追加

--- server_test.go
+++ server_test.go
@@ -59,6 +59,25 @@ func TestGETPlayers(t *testing.T) {
 
 		assertStatus(t, response.Code, http.StatusNotFound)
 	})
+
+	t.Run("it records wins on POST", func(t *testing.T) {
+		player := "Pepper"
+
+		request := newPostWinRequest(player)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+
+		if len(store.winCalls) != 1 {
+			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
+		}
+	})
 }
 
 func TestStoreWins(t *testing.T) {
