160bbd510f85e6e080b47e8a2178c3f359a5eccf
GenkiHirano <genki86web@gmail.com>
2022-08-04 17:06:53 +0900

    refactor: TestFileSystemStoreをリファクタリング

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -28,20 +28,15 @@ func TestFileSystemStore(t *testing.T) {
 		assertLeague(t, got, want)
 	})
 
-	t.Run("get player store", func(t *testing.T) {
+	t.Run("get player score", func(t *testing.T) {
 		database := strings.NewReader(`[
 			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}
-		]`)
+			{"Name": "Chris", "Wins": 33}]`)
 
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Chris")
-
 		want := 33
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertScoreEquals(t, got, want)
 	})
 }
