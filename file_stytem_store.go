f1aa239c374e64a03a54247501165dd1bec7fce2
GenkiHirano <genki86web@gmail.com>
2022-08-04 11:38:29 +0900

    feat: FileSystemPlayerStoreを実装

--- file_stytem_store.go
+++ file_stytem_store.go
@@ -1,3 +1,16 @@
 package main
 
-type FileSystemPlayerStore struct {}
+import (
+	"encoding/json"
+	"io"
+)
+
+type FileSystemPlayerStore struct {
+	database io.Reader
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	var league []Player
+	json.NewDecoder(f.database).Decode(&league)
+	return league
+}
