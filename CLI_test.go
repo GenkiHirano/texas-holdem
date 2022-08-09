e9d38a198db2633d5656044ecb2a619c7ae4ea52
GenkiHirano <genki86web@gmail.com>
2022-08-09 10:54:49 +0900

    test: 強制ベッドに関する処理の置き場所を整理

--- CLI_test.go
+++ CLI_test.go
@@ -3,16 +3,33 @@ package poker_test
 import (
 	"strings"
 	"testing"
+	"time"
 
 	poker "github.com/GenkiHirano/tdd-go.git"
 )
+
+type SpyBlindAlerter struct {
+	alerts []struct {
+		scheduledAt time.Duration
+		amount      int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduledAt time.Duration
+		amount      int
+	}{duration, amount})
+}
+
+var dummySpyAlerter = &SpyBlindAlerter{}
 
 func TestCLI(t *testing.T) {
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Chris")
@@ -22,7 +39,7 @@ func TestCLI(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
 		playerStore := &poker.StubPlayerStore{}
 
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
@@ -31,7 +48,7 @@ func TestCLI(t *testing.T) {
 	t.Run("it schedules printing of blind values", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &poker.StubPlayerStore{}
-		blindAlerter := &poker.SpyBlindAlerter{}
+		blindAlerter := &SpyBlindAlerter{}
 
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
