8f8876404abb7dee84ef8d08c93bac8f3d94ddf5
GenkiHirano <genki86web@gmail.com>
2022-08-12 17:08:38 +0900

    refactor: GameをTexasHoldemに名称変更し、リファクタリング

--- CLI_test.go
+++ CLI_test.go
@@ -10,6 +10,9 @@ import (
 	poker "github.com/GenkiHirano/tdd-go.git"
 )
 
+var dummyBlindAlerter = &spyBlindAlerter{}
+var dummyPlayerStore = &poker.StubPlayerStore{}
+
 type scheduledAlert struct {
 	at     time.Duration
 	amount int
@@ -19,22 +22,33 @@ func (s scheduledAlert) String() string {
 	return fmt.Sprintf("%d chips at %v", s.amount, s.at)
 }
 
-type SpyBlindAlerter struct {
+type spyBlindAlerter struct {
 	alerts []scheduledAlert
 }
 
-func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+func (s *spyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
 	s.alerts = append(s.alerts, scheduledAlert{duration, amount})
 }
 
-var dummyPlayerStore = &poker.StubPlayerStore{}
+type GameSpy struct {
+	StartedWith  int
+	FinishedWith string
+}
+
+func (g *GameSpy) Start(numberOfPlayers int) {
+	g.StartedWith = numberOfPlayers
+}
+
+func (g *GameSpy) Finish(winner string) {
+	g.FinishedWith = winner
+}
 
 func TestCLI(t *testing.T) {
 	t.Run("it prompts the user to enter the number of players", func(t *testing.T) {
 		stdout := &bytes.Buffer{}
 		in := strings.NewReader("7\n")
-		blindAlerter := &SpyBlindAlerter{}
-		game := poker.NewGame(blindAlerter, dummyPlayerStore)
+		blindAlerter := &spyBlindAlerter{}
+		game := poker.NewTexasHoldem(blindAlerter, dummyPlayerStore)
 
 		cli := poker.NewCLI(in, stdout, game)
 		cli.PlayPoker()
@@ -64,64 +78,6 @@ func TestCLI(t *testing.T) {
 			})
 		}
 	})
-
-	// t.Run("it schedules printing of blind values", func(t *testing.T) {
-	// 	in := strings.NewReader("Chris wins\n")
-	// 	playerStore := &poker.StubPlayerStore{}
-	// 	blindAlerter := &SpyBlindAlerter{}
-	// 	game := poker.NewGame(blindAlerter, playerStore)
-
-	// 	cli := poker.NewCLI(in, dummyStdOut, game)
-	// 	cli.PlayPoker()
-
-	// 	cases := []scheduledAlert{
-	// 		{0 * time.Second, 100},
-	// 		{10 * time.Minute, 200},
-	// 		{20 * time.Minute, 300},
-	// 		{30 * time.Minute, 400},
-	// 		{40 * time.Minute, 500},
-	// 		{50 * time.Minute, 600},
-	// 		{60 * time.Minute, 800},
-	// 		{70 * time.Minute, 1000},
-	// 		{80 * time.Minute, 2000},
-	// 		{90 * time.Minute, 4000},
-	// 		{100 * time.Minute, 8000},
-	// 	}
-
-	// 	for i, want := range cases {
-	// 		t.Run(fmt.Sprint(want), func(t *testing.T) {
-
-	// 			if len(blindAlerter.alerts) <= i {
-	// 				t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
-	// 			}
-
-	// 			got := blindAlerter.alerts[i]
-	// 			assertScheduledAlert(t, got, want)
-	// 		})
-	// 	}
-	// })
-
-	// t.Run("record chris win from user input", func(t *testing.T) {
-	// 	in := strings.NewReader("Chris wins\n")
-	// 	playerStore := &poker.StubPlayerStore{}
-	// 	game := poker.NewGame(dummySpyAlerter, playerStore)
-
-	// 	cli := poker.NewCLI(in, dummyStdOut, game)
-	// 	cli.PlayPoker()
-
-	// 	poker.AssertPlayerWin(t, playerStore, "Chris")
-	// })
-
-	// t.Run("record cleo win from user input", func(t *testing.T) {
-	// 	in := strings.NewReader("Cleo wins\n")
-	// 	playerStore := &poker.StubPlayerStore{}
-	// 	game := poker.NewGame(dummySpyAlerter, playerStore)
-
-	// 	cli := poker.NewCLI(in, dummyStdOut, game)
-	// 	cli.PlayPoker()
-
-	// 	poker.AssertPlayerWin(t, playerStore, "Cleo")
-	// })
 }
 
 func assertScheduledAlert(t testing.TB, got, want scheduledAlert) {
